4f31cfb4611ec9d7227246a2aba7f6852ffc6fd4
loly1710 <loly15710@gmail.com>
2024-10-20 16:06:32 +0300

    finished lecture

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
+const session = require('express-session');
 require('dotenv').config();
 require('./config/database');
 
-const app = express();
+// Controllers
+const authController = require('./controllers/auth');
 
+const app = express();
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : '3000';
+
+// MIDDLEWARE
 
 // Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
@@ -15,16 +20,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // Morgan for logging HTTP requests
 app.use(morgan('dev'));
-//Routes
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
+// public ROUTES
+app.get('/', async (req, res) => {
+  res.render('index.ejs', { user: req.session.user });
+});
+
+app.use('/auth', authController);
+
+// protected routes
+app.get("/vip-lounge", async(req, res) => {
+    if (req.session.user) {
+      res.send(`Welcome to the party ${req.session.user.username}.`);
+    } else {
+      res.send("Sorry, no guests allowed.");
+    }
+  });
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`The express app is ready on port ${port}!`);
 });
 
-// server.js
 
-// GET /
-app.get("/", async (req, res) => {
+  /*app.get("/", async (req, res) => {
     res.send("hello, friend!");
-  });
+  });*/
